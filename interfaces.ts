480da8f25a7b7465337b517a4f6b19d8538fa67a
AndreyPov <povoroznuka.m@gmail.com>
2017-02-18 01:49:54 +0200

    all about interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -80,7 +80,78 @@ interface StringArray {             //add indexes to array
 }
 
 let myArray: StringArray;
-myArray = ["Bob", "Fred"];           ///extremely
+myArray = ["Bob", "Fred"];
 
 let myStr: string = myArray[0];
 console.log(myStr);
+
+////////////////////
+
+interface ClockConstructor {
+    new (hour: number, minute: number): ClockInterface;
+}
+interface ClockInterface {
+    tick();
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("beep beep");
+    }
+}
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("tick tock");
+    }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+let analog = createClock(AnalogClock, 7, 32);
+digital.tick();
+
+//////extending interfaces
+
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+let square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
+
+//////////////
+
+interface Counter {
+    (start: number): string;   ///hybrid type
+    interval: number;
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    console.log(counter);
+    return counter;
+}
+
+let c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
+console.log(c);
