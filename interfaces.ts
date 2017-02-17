d5dd6f558ceb3a89e5d41ab0f7eacb263e0d9c6e
AndreyPov <povoroznuka.m@gmail.com>
2017-02-17 17:34:14 +0200

    interfaces: indexes, return type, etc

--- interfaces.ts
+++ interfaces.ts
@@ -29,7 +29,7 @@ function createSquare(config: SquareConfig): { color: string, area: number} {
   else if (config.area){
     newSquare.area = config.area;
   }
-  else if (config.widtj){
+  else if (config.width){
     newSquare.area = config.width*config.width;
   }
   console.log(newSquare);
@@ -49,3 +49,38 @@ let p: Point = {x:2, y:3}; //after this operation we cannot assign p.x=4;
 
 let arr: number[] = [2,3,4];
 let ro: ReadonlyArray<number> = arr; //after this operation we cannot change ro[];
+
+/////////////
+
+interface CircleConfig {
+    color?: string;                //we can add color with string type only or we may not add it at all
+    diameter?: number;             //the same situation
+    [propName: string]: any;       //there could be any number of other properties with string type
+}
+/////////////
+
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function(src, sub) {
+    let result = src.search(sub);
+    result = result > -1;
+    console.log(result);
+    return result;
+}
+
+mySearch("I have no idea what is in here.", "hat");
+
+///////////////////
+
+interface StringArray {             //add indexes to array
+  [index: number]: string;
+}
+
+let myArray: StringArray;
+myArray = ["Bob", "Fred"];           ///extremely
+
+let myStr: string = myArray[0];
+console.log(myStr);
